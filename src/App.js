90b3f0f59de16b11337b9ac579dd557b407ef963
jimmoua1 <tsheejmaus@yahoo.com>
2020-07-12 02:34:48 -0700

    files incorrect

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import SignInandSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import { auth, createUserProfileDocument } from './components/firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 
-class App extends React.Component{
+class App extends React.Component {
   unsubscribeFromAuth = null;
 
-  componentDidMount(){
-    const {setCurrentUser}  = this.props
+  componentDidMount() {
+    const { setCurrentUser } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -23,13 +23,13 @@ class App extends React.Component{
 
         userRef.onSnapshot(snapShot => {
           setCurrentUser({
-              id: snapShot.id,
-              ...snapShot.data()
-            });
+            id: snapShot.id,
+            ...snapShot.data()
+          });
         });
       }
 
-      this.setState(userAuth);
+      setCurrentUser(userAuth);
     });
   }
 
@@ -44,7 +44,7 @@ class App extends React.Component{
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInandSignUpPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
         </Switch>
       </div>
     );
@@ -55,4 +55,7 @@ const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(
+  null,
+  mapDispatchToProps
+)(App);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,15 +3,17 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyAo78OvSocLaf3eADj-5t4cSNzAa1rUpps",
-    authDomain: "ecommerceclothes-4ca4e.firebaseapp.com",
-    databaseURL: "https://ecommerceclothes-4ca4e.firebaseio.com",
-    projectId: "ecommerceclothes-4ca4e",
-    storageBucket: "ecommerceclothes-4ca4e.appspot.com",
-    messagingSenderId: "601231516774",
-    appId: "1:601231516774:web:4fe5b96980c7171c71f9a7",
-    measurementId: "G-7GXCWRLNPW"
-  }
+  apiKey: "AIzaSyAo78OvSocLaf3eADj-5t4cSNzAa1rUpps",
+  authDomain: "ecommerceclothes-4ca4e.firebaseapp.com",
+  databaseURL: "https://ecommerceclothes-4ca4e.firebaseio.com",
+  projectId: "ecommerceclothes-4ca4e",
+  storageBucket: "ecommerceclothes-4ca4e.appspot.com",
+  messagingSenderId: "601231516774",
+  appId: "1:601231516774:web:4fe5b96980c7171c71f9a7",
+  measurementId: "G-7GXCWRLNPW"
+};
+
+firebase.initializeApp(config);
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
@@ -20,17 +22,16 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
   const snapShot = await userRef.get();
 
-  if(!snapShot.exists) {
+  if (!snapShot.exists) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
-
     try {
       await userRef.set({
         displayName,
         email,
         createdAt,
         ...additionalData
-      })
+      });
     } catch (error) {
       console.log('error creating user', error.message);
     }
@@ -38,10 +39,6 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
   return userRef;
 };
-
-
-
-firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
